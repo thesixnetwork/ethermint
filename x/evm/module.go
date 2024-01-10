bece842d6a32aabd4d5be2fdb42427e2e0cd94a3
dDee-D-six <dee.hamdee.084@gmail.com>
2024-01-10 18:26:41 +0700

    :truck: revoke migration

--- x/evm/module.go
+++ x/evm/module.go
@@ -44,7 +44,7 @@ func (AppModuleBasic) RegisterLegacyAminoCodec(_ *codec.LegacyAmino) {
 
 // ConsensusVersion returns the consensus state-breaking version for the module.
 func (AppModuleBasic) ConsensusVersion() uint64 {
-	return 2
+	return 3
 }
 
 // DefaultGenesis returns default genesis state as raw bytes for the evm
@@ -123,15 +123,15 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterMsgServer(cfg.MsgServer(), am.keeper)
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 
-	m := keeper.NewMigrator(*am.keeper)
-	err := cfg.RegisterMigration(types.ModuleName, 1, m.Migrate1to2)
-	if err != nil {
-		panic(err)
-	}
-	err = cfg.RegisterMigration(types.ModuleName, 2, m.Migrate2to3)
-	if err != nil {
-		panic(err)
-	}
+	_ = keeper.NewMigrator(*am.keeper)
+	// err := cfg.RegisterMigration(types.ModuleName, 1, m.Migrate1to2)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// err = cfg.RegisterMigration(types.ModuleName, 2, m.Migrate2to3)
+	// if err != nil {
+	// 	panic(err)
+	// }
 }
 
 // Route returns the message routing key for the evm module.
